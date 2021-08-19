8df46e4257f4f61d68fd1f8a8676ae9509c6085a
Aleksandr <menshikov.a.v@icloud.com>
2021-08-19 21:57:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import csv
-import re
-import requests
-import pprint
-
 from pathlib import Path
+
+import requests
 from bs4 import BeautifulSoup
 
 
 def get_site_access(url):
+    """Проверяет корректность ссылки возвращает объект класса BeautifulSoup."""
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -18,19 +17,23 @@ def get_site_access(url):
 
 
 def get_recipes_links(eda_url):
+    """Возвращает ссылки на блюда."""
     soup = get_site_access(eda_url)
 
-    dishes = soup.find_all(class_='horizontal-tile__item-title item-title')
+    dishes = soup.find_all('div', class_='card__description')
     recipes_links = {}
     for dish in dishes:
-        dish_title = re.sub(r'^\s+|\n|\r|\s+$', '', dish.find('span').text)
+        dish_title = dish.find('div', class_='card__title title').text
         dish_link = dish.find('a').get('href')
-        recipes_links[re.sub(r'\xa0', ' ', dish_title)] = f'https://eda.ru{dish_link}'
+        recipes_links[dish_title] = f'https://www.edimdoma.ru{dish_link}'
     return recipes_links
 
 
 def get_recipe(title, link):
+    """Формирует csv файл с ингредиентами и txt файл с рецептом"""
     soup = get_site_access(link)
+    # На сколько порций рассчитано
+    portions = soup.find(class_='field__container').find(attrs={'name': 'servings'})['value']
     # Записываем заголовки столбцов
     with open(f'recipes/{title}.csv', 'a', encoding='utf-8') as file:
         writer = csv.writer(file)
@@ -38,17 +41,18 @@ def get_recipe(title, link):
             (
                 'Ингредиенты',
                 'Количество',
-                'Цена'
+                'Цена',
+                f'Количество порций: {portions}'
             )
         )
 
-    products = soup.find_all(class_='css-ipetvh-Column')
+    products = soup.find(class_='field-row recipe_ingredients').find_all(class_='definition-list-table')
     # Перебираем продукты из списка ингредиентов
     for product in products:
-        product_title = product.find(class_='css-12s4kyf-Info').find('span').text
-        product_count = product.find(class_='css-1t5teuh-Info').text
-        product_price = 0 # Здесь вызов функция с ценой
-
+        product_title = product.find(class_='recipe_ingredient_title').text
+        product_count = product.find(class_='definition-list-table__td definition-list-table__td_value').text
+        product_price = 0  # Здесь вызов функция с ценой
+        # Записываем ингредиент и его количество в таблицу csv
         with open(f'recipes/{title}.csv', 'a', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(
@@ -59,16 +63,17 @@ def get_recipe(title, link):
                 )
             )
     # Создаем txt файл с инструкцией по приготовлению
-    cooking_steps = soup.find_all(class_='css-18thmuh-Column')
+    cooking_steps = soup.find_all(class_='plain-text recipe_step_text')
     with open(f'recipes/{title}.txt', 'w') as file:
-        for step in cooking_steps:
-            file.write(f'{step.text}\n')
+        for number, step in enumerate(cooking_steps, 1):
+            file.write(f'{number}. {step.text}\n\n')
 
 
 def main():
     Path('recipes').mkdir(parents=True, exist_ok=True)
-    eda_url = 'https://eda.ru/recepty'
-    recipes_links = get_recipes_links(eda_url)
+
+    edimdoma_url = 'https://www.edimdoma.ru/retsepty?tags%5Brecipe_mealtime%5D%5B%5D=%D0%B7%D0%B0%D0%B2%D1%82%D1%80%D0%B0%D0%BA&with_ingredient=&with_ingredient_condition=and&without_ingredient=&user_ids=&field=&direction=&query='
+    recipes_links = get_recipes_links(edimdoma_url)
     for dish_title, dish_link in recipes_links.items():
         get_recipe(dish_title, dish_link)
 
