1dd92f467ec121a5b5fc4a66450fb4dd3a046294
Aleksandr <menshikov.a.v@icloud.com>
2021-08-20 16:56:59 +0300

    Add downloading recipes by category

--- main.py
+++ main.py
@@ -29,13 +29,14 @@ def get_recipes_links(eda_url):
     return recipes_links
 
 
-def get_recipe(title, link):
+def get_recipe(title, link, meal):
     """Формирует csv файл с ингредиентами и txt файл с рецептом"""
     soup = get_site_access(link)
+    Path(f'recipes/{meal}').mkdir(parents=True, exist_ok=True)
     # На сколько порций рассчитано
     portions = soup.find(class_='field__container').find(attrs={'name': 'servings'})['value']
     # Записываем заголовки столбцов
-    with open(f'recipes/{title}.csv', 'a', encoding='utf-8') as file:
+    with open(f'recipes/{meal}/{title}.csv', 'a', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(
             (
@@ -53,7 +54,7 @@ def get_recipe(title, link):
         product_count = product.find(class_='definition-list-table__td definition-list-table__td_value').text
         product_price = 0  # Здесь вызов функция с ценой
         # Записываем ингредиент и его количество в таблицу csv
-        with open(f'recipes/{title}.csv', 'a', encoding='utf-8') as file:
+        with open(f'recipes/{meal}/{title}.csv', 'a', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(
                 (
@@ -64,18 +65,25 @@ def get_recipe(title, link):
             )
     # Создаем txt файл с инструкцией по приготовлению
     cooking_steps = soup.find_all(class_='plain-text recipe_step_text')
-    with open(f'recipes/{title}.txt', 'w') as file:
+    with open(f'recipes/{meal}/{title}.txt', 'w') as file:
         for number, step in enumerate(cooking_steps, 1):
             file.write(f'{number}. {step.text}\n\n')
 
 
+def get_recipes_by_category(meals):
+    for meal in meals:
+        edimdoma_url = f'https://www.edimdoma.ru/retsepty?tags%5Brecipe_mealtime%5D%5B%5D={meal}&with_ingredient=&with_ingredient_condition=and&without_ingredient=&user_ids=&field=&direction=&query='
+        recipes_links = get_recipes_links(edimdoma_url)
+        for dish_title, dish_link in recipes_links.items():
+            get_recipe(dish_title, dish_link, meal)
+        print(f'Обработка категории {meal} завершена.')
+    print('Все категории обработаны!')
+
+
 def main():
     Path('recipes').mkdir(parents=True, exist_ok=True)
-
-    edimdoma_url = 'https://www.edimdoma.ru/retsepty?tags%5Brecipe_mealtime%5D%5B%5D=%D0%B7%D0%B0%D0%B2%D1%82%D1%80%D0%B0%D0%BA&with_ingredient=&with_ingredient_condition=and&without_ingredient=&user_ids=&field=&direction=&query='
-    recipes_links = get_recipes_links(edimdoma_url)
-    for dish_title, dish_link in recipes_links.items():
-        get_recipe(dish_title, dish_link)
+    meals = ['завтрак', 'обед', 'ужин']
+    get_recipes_by_category(meals)
 
 
 if __name__ == '__main__':
